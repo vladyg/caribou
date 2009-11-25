facd1aac3d2480309cbe1eb6fa7b1107016a584e
Ben Konrath <ben@bagu.org>
2009-11-25 15:45:31 -0500

    swap space and . keys in qwerty keyboard

--- src/qwerty.py
+++ src/qwerty.py
@@ -57,15 +57,15 @@ lt = ("abc", "lowercase")
 # keyboard layouts - put a single utf-8 character or a tuple defined above
 lowercase = ( ("?", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p"),
               ( np, "a", "s", "d", "f", "g", "h", "j", "k", "l",  bs),
-              ( su, "z", "x", "c", "v", "b", "n", "m", ".",  sp,  en) )
+              ( su, "z", "x", "c", "v", "b", "n", "m", sp,  ".",  en) )
 
 uppercase = ( ("?", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"),
               ( np, "A", "S", "D", "F", "G", "H", "J", "K", "L",  bs),
-              ( sd, "Z", "X", "C", "V", "B", "N", "M", ".",  sp,  en) )
+              ( sd, "Z", "X", "C", "V", "B", "N", "M",  sp, ".",  en) )
 
 num_punct = ( ("!", "1", "2", "3", "4", "5", "6",  "7",  "8", "9", "0"),
               ( lt, "@", "$", "/", "+", "-",  up, "\"",  ",", "#",  bs),
-              ("'", "(", ")", ";", ":",  le,  dn,   ri,  ".",  sp,  en) )
+              ("'", "(", ")", ";", ":",  le,  dn,   ri,   sp, ".",  en) )
 
 # the layout that appears in position 0 will be visible
 # when the keyboard is first created
