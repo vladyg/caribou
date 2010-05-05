debe66c357306fc0050c31c14cd70354252539e6
Javier Jardón <jjardon@gnome.org>
2010-05-05 15:40:59 +0200

    Update autotools configuration
    New requirements:
    automake >= 1.11
    autoconf >= 2.63
    
    Also, added pygtk and pyclutter as required dependencies

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,21 @@
+#!/bin/sh
 # Calls gnome-autogen to build Makefiles and run configure
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-REQUIRED_AUTOMAKE_VERSION=1.7.2
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+PKG_NAME="caribou"
+
+(test -f $srcdir/configure.ac) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common module and make"
+    echo "sure the gnome-autogen.sh script is in your \$PATH."
+    exit 1
+}
+
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
